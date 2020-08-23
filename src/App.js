c0d468c5775f156d3a9b0fdb45a6cf90b13601ed
TOMO-YOSHI <lucky.paddy10@gmail.com>
2020-08-23 01:08:53 -0700

    finishing sign in handleSubmit method

--- src/App.js
+++ src/App.js
@@ -31,13 +31,7 @@ class App extends React.Component {
               id: snapShot.id,
               ...snapShot.data()
             }
-          }
-          // , () => {
-          //     console.log(this.state);
-          // }
-          );
-
-          console.log(this.state);
+          });
         });
       } else {
         this.setState({ currentUser: userAuth });
